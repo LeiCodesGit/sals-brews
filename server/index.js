96b26a4cc90120d311d0880c759036f1751cb502
sami <sophiamanagbanag@gmail.com>
2025-07-04 05:36:34 +0800

    the user can edit their profile

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ import { dirname } from "path";
 import dotenv from "dotenv";
 import connect from "./database/mongodb-connect.js";
 import  session from "express-session";
+import userRouter from "./routes/users.js";
+import mongoose from "mongoose";
+import authRouter from "./routes/auth/routes.js";
+import adminRouter from "./routes/admin.js";
 
 // Load environment variables
 dotenv.config();
@@ -14,17 +18,20 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 4000;
+
+connect();
 
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({credentials: true}));
 
-// Session management
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "defaultsecret",
     resave: false,
     saveUninitialized: false,
     cookie: {
@@ -38,6 +45,7 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use("/styles", express.static(path.join(__dirname, "styles")));
 app.use("/scripts", express.static(path.join(__dirname, "scripts")));
 app.use(express.static(path.join(__dirname, "public"))); 
+
 //set Ejs as the templating engine
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -50,8 +58,7 @@ app.get("/itempage", (req, res) => {
 });
 
 // Routes
-import authRouter from "./routes/auth/routes.js";
-import adminRouter from "./routes/admin.js";
+
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
@@ -66,15 +73,6 @@ app.get('/menu', (req, res) => {
     res.render('users/menu');
 });
 
-app.get('/', (req, res) => {
-    res.render('users/home');
-});
-
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-});
-
 app.get('/cart', (req, res) => {
     res.render('users/cart');
 });
@@ -83,9 +81,10 @@ app.get('/orderinfo', (req, res) => {
     res.render('users/orderinformation');
 });
 
-app.get('/profile', (req, res) => {
-    res.render('users/profile');
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
+
 
 
 
